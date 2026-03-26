43bc6217f5f60996faa21a618d337d879e418e5f
ronakrpanchal <rhtpanchal76@gmail.com>
2026-03-26 14:03:50 +0530

    grwm for deployment

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(SessionMiddleware, secret_key=settings.FASTAPI_KEY)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
